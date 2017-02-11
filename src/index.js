b4e607888787d024816c038eb28775b635998c38
Hugh Rawlinson <hugh@spotify.com>
2017-02-11 14:51:29 +0100

    Get rewrites working

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
+import template from 'babel-template';
+
+const prepareConsequent = (t, node) => {
+  if (t.isBlockStatement(node)) {
+    if (!t.isExpressionStatement(node.body[node.body.length-1])) {
+      return null;
+    } else {
+      return t.blockStatement(
+        node.body
+            .slice(0,-1)
+            .concat([
+              t.returnStatement(node.body[node.body.length-1].expression)
+            ]))
+    }
+  } else if (t.isExpressionStatement(node)) {
+    return t.returnStatement(node.expression);
+  } else {
+    return null;
+  }
+};
+
+const prepareAlternate = (t, node) => {
+  if (node) {
+    if (node.type === "IfStatement") {
+      return prepareIf(t,node);
+    } else {
+      const a = prepareConsequent(t,node);
+      return !a ? null : a;
+    }
+  } else {
+    return null;
+  }
+}
+
+const prepareIf = (t, node) => {
+  return Object.assign({
+      shouldSkip: true
+    },
+    t.ifStatement(
+      node.test,
+      prepareConsequent(t,node.consequent),
+      prepareAlternate(t,node.alternate))
+  );
+};
+
 export default function({
   types: t
 }) {
   return {
     visitor: {
       IfStatement(path, state) {
-        path.replaceWith(
-          t.expressionStatement(
-            t.callExpression(
-              t.arrowFunctionExpression(
-                [],
-                t.blockStatement(
-                  [
-                    t.returnStatement(
-                      t.stringLiteral("Got Here")
-                    )
-                  ]
-                )
-              ),
-            [])
+        if (!path.node.shouldSkip){
+          path.replaceWith(
+            template(`(()=>{BODY})()`)({
+              BODY: prepareIf(t, path.node)
+            })
           )
-        );
-      },
+        }
+      }
     }
   };
 };
--- test/index.js
+++ test/index.js
@@ -2,10 +2,12 @@ import test from 'ava';
 import {transform} from 'babel-core';
 import ifExpress from '../dist/index.js';
 
-test(t => {
-  const result = transform("{if(true){}}", {
-    plugins: [ ifExpress ]
-  });
+const applyExpressIf = code => transform(code, {
+  plugins: [ifExpress]
+});
+
+test.skip(t => {
+  const result = applyExpressIf("{if(true){}}");
 
   const expectedResult = `{
   (() => {
@@ -14,4 +16,52 @@ test(t => {
 }`;
 
   t.is(result.code,expectedResult);
+});
+
+test("Rewrites IfStatement without alternate", t => {
+  const result = applyExpressIf("{if(true){true}}");
+
+  const expectedResult = `{
+  (() => {
+    if (true) {
+      return true;
+    }
+  })();
+}`;
+
+  t.is(result.code,expectedResult);
+});
+
+test("Rewrites IfStatement with alternate", t => {
+  const result = applyExpressIf("{if(true){true}else{false}}");
+
+  const expectedResult = `{
+  (() => {
+    if (true) {
+      return true;
+    } else {
+      return false;
+    }
+  })();
+}`;
+
+  t.is(result.code,expectedResult);
+});
+
+test("Rewrites IfStatement with IfStatement Alternate", t => {
+  const result = applyExpressIf("{if(true){true}else if(true){true}else{true}}");
+
+  const expectedResult = `{
+  (() => {
+    if (true) {
+      return true;
+    } else if (true) {
+      return true;
+    } else {
+      return true;
+    }
+  })();
+}`
+
+  t.is(result.code, expectedResult);
 });
